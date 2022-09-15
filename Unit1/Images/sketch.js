3d61c41fe087fa2acf0c10a8c367e9d0aad04b78
Mattrhea24 <89150716+Mattrhea24@users.noreply.github.com>
2022-09-15 12:07:30 -0500

    Update sketch.js

--- Unit1/Images/sketch.js
+++ Unit1/Images/sketch.js
@@ -1,18 +1,1 @@
 
-let i1, i2, i3;
-
-
-function setup() {
-  createCanvas(500, 500);
-  i1 = loadImage("assets/art1.jpg")
-  i2 = loadImage("assets/art3.jpg")
-  i3 = loadImage("assets/art3.jpg")
-  imageMode(CENTER);
-}
-
-function draw() {
-  background("grey");
-  image(i1, width/ 2, height/ 2 - 120, 100, 100);
-  image(i2, width/ 2, height/ 2, 100, 100);
-  image(i3, width/ 2, height/ 2 + 120, 100, 100);
-}
