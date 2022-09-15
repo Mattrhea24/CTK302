00b1e7d234c5e70d590aa19815b1b8cb60917a38
Mattrhea24 <89150716+Mattrhea24@users.noreply.github.com>
2022-09-15 12:10:15 -0500

    modified:   Unit1/Images/sketch.js 	new file:   Unit1/fonts/assets/Lemon-Regular.ttf 	new file:   Unit1/fonts/assets/Nabla-Regular-VariableFont_EDPT,EHLT.ttf     new file:   Unit1/fonts/assets/OFL.txt 	new file:   Unit1/fonts/assets/README.txt

--- Unit1/Images/sketch.js
+++ Unit1/Images/sketch.js
@@ -3,14 +3,15 @@ let i1, i2, i3;
 
 function setup() {
   createCanvas(500, 500);
-  i1 = loadImage("assets/art1.jpeg")
-  i2 = loadImage("assets/art2.jpeg")
-  i3 = loadImage("assets/art3.jpeg")
+  i1 = loadImage("assets/art1.jpg")
+  i2 = loadImage("assets/art2.jpg")
+  i3 = loadImage("assets/art3.jpg")
+  imageMode(CENTER);
 }
 
 function draw() {
   background("grey");
   image(i1, width/ 2, height/ 2 - 120, 100, 100);
   image(i2, width/ 2, height/ 2, 100, 100);
-  image(i2, width/ 2, height/ 2 + 120, 100, 100);
+  image(i3, width/ 2, height/ 2 + 120, 100, 100);
 }
