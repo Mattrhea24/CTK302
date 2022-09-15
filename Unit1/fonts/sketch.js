a9641ac23b13a21b05d991b7b4e483cb8a37ca4b
Mattrhea24 <89150716+Mattrhea24@users.noreply.github.com>
2022-09-15 12:49:08 -0500

    new file:   .vscode/launch.json 	renamed:    Unit1/fonts/assets/Lemon-Regular.ttf -> Unit1/fonts/assets/lemon.ttf     renamed:    Unit1/fonts/assets/Nabla-Regular-VariableFont_EDPT,EHLT.ttf -> Unit1/fonts/assets/nabla.ttf     modified:   Unit1/fonts/sketch.js

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,7 +1,18 @@
+let f1, f2, f3 ;
+
+
 function setup() {
   createCanvas(500, 500);
+  f1 = loadFont("assets/lemon.ttf");
+  f2 = loadFont("assets/nabla.ttf");
+  textAlign(CENTER);
 }
 
 function draw() {
-
+  background("black");
+  fill("white");
+  textFont(f1, 48);
+  text("Hello there!", width / 2, 100);
+  textFont(f2, 48);
+  text("What's up?", width / 2, 200 );
 }
