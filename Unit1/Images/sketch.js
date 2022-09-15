7b4cdf40f6eb2b38beb5e2ccfb08ad2bbfc849bc
Mattrhea24 <89150716+Mattrhea24@users.noreply.github.com>
2022-09-15 12:03:21 -0500

    Update sketch.js

--- Unit1/Images/sketch.js
+++ Unit1/Images/sketch.js
@@ -6,7 +6,7 @@ function setup() {
   createCanvas(500, 500);
   i1 = loadImage("assets/art1.jpg")
   i2 = loadImage("assets/art2.jpg")
-  i3 = loadImage("assets/art1.jpg")
+  i3 = loadImage("assets/art3.jpg")
   imageMode(CENTER);
 }
 
